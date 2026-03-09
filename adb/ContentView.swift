102073f954872834d15026b5e199092c982294b3
Kazuki <Arimoto>
2026-03-09 20:35:26 +0900

    Initial Commit

---
+++ adb/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  adb
+//
+//  Created by Kazuki Arimoto on 2026/03/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ adb/adbApp.swift
@@ -0,0 +1,17 @@
+//
+//  adbApp.swift
+//  adb
+//
+//  Created by Kazuki Arimoto on 2026/03/09.
+//
+
+import SwiftUI
+
+@main
+struct adbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
